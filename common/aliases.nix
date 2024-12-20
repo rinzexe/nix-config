5521d4051cb9dab608264a553a9b07ea19f9e6bd
rinz <roberts.denisovs@gmail.com>
2024-12-20 06:25:13 +0200

    impure

--- common/aliases.nix
+++ common/aliases.nix
@@ -1,7 +1,7 @@
 {
   environment.shellAliases = {
     ll = "ls -l";
-    rebuild = "sudo nixos-rebuild switch --flake github:rinzexe/nix-config#default --no-write-lock-file";
+    rebuild = "sudo nixos-rebuild switch --flake github:rinzexe/nix-config#default --impure --no-write-lock-file";
     flake-cuda = "nix develop github:rinzexe/nix-flakes/cuda";
   };
 }
