5b65feda04ba599c6a66729b274d421678abdfea
rinz <roberts.denisovs@gmail.com>
2024-12-20 08:09:31 +0200

    please man

--- flake.nix
+++ flake.nix
@@ -8,21 +8,26 @@
   outputs = { self, nixpkgs }: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = 
-      let hostname = builtins.getEnv "HOSTNAME"; in
-      [
-        # fixes a weird bug, don't remove
-                ({ ... }: {
-          nixpkgs.config.permittedInsecurePackages = [
-            "dotnet-sdk-6.0.428"
-            "dotnet-runtime-6.0.36"
-          ];
-        })
-        ./common/aliases.nix
-        ./common/config.nix
-        
-        (if hostname == "rinz-main" then ./main/configuration.nix else "")
-      ];
+
+      modules = let
+        hostname = builtins.getEnv "HOSTNAME";
+        moduleList = [
+          # fixes a weird bug, don't remove
+          ({ ... }: {
+            nixpkgs.config.permittedInsecurePackages = [
+              "dotnet-sdk-6.0.428"
+              "dotnet-runtime-6.0.36"
+            ];
+          })
+          ./common/aliases.nix
+          ./common/config.nix
+
+          # Include specific configuration based on hostname
+          (if hostname == "rinz-main" then ./main/configuration.nix else null)
+        ];
+      in
+        # Filter out null values
+        builtins.filter (module: module != null) moduleList;
     };
   };
 }
