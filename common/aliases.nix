59b06713095d46fcf2b29ac5fb6b3d6193f3cede
rinz <roberts.denisovs@gmail.com>
2024-12-20 06:09:49 +0200

    locked

--- common/aliases.nix
+++ common/aliases.nix
@@ -1,7 +1,7 @@
 {
   environment.shellAliases = {
     ll = "ls -l";
-    rebuild = "sudo nixos-rebuild switch --flake github:rinzexe/nix-config";
-    flake-cuda = "nix develop github:rinzexe/nix-flakes/cuda"
+    rebuild = "sudo nixos-rebuild switch --flake github:rinzexe/nix-config#default";
+    flake-cuda = "nix develop github:rinzexe/nix-flakes/cuda";
   };
 }
