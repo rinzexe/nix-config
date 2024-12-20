b86836dbe9900182eb35a2a0a48b93a1bcaec10c
rinz <roberts.denisovs@gmail.com>
2024-12-20 06:22:48 +0200

    fix

--- common/aliases.nix
+++ common/aliases.nix
@@ -1,7 +1,7 @@
 {
   environment.shellAliases = {
     ll = "ls -l";
-    rebuild = "sudo nixos-rebuild switch --flake github:rinzexe/nix-config#default";
+    rebuild = "sudo nixos-rebuild switch --flake github:rinzexe/nix-config#default --impure";
     flake-cuda = "nix develop github:rinzexe/nix-flakes/cuda";
   };
 }
